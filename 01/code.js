64c3d347f6f7da8915acc940174a2b998bf5ba53
Callum Robertson <callumullac@hotmail.com>
2022-12-01 18:10:30 +1100

    Create code.js

---
+++ 01/code.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+
+const solve = (inputFile) => {
+  try {
+    const data = fs.readFileSync(inputFilePath, 'utf8');
+    
+    // an integer followed by either a newline or the end of the file
+    const itemPattern = new RegExp('\\d+(\\n|$)');
+    
+    // an inventory is just multiple items in a sequence without any line break between them
+    const inventoryPattern = new RegExp(`(${itemPattern.source})+`;
+    
+    return data.match(inventoryPattern)
+      .map(inventory => 
+           inventory.match(itemPattern)
+              .map(item) => parseInt(item))
+              .reduce((a,b) => a + b))
+      .reduce((a,b) => Math.max(a,b))
+  } catch (err) {
+    console.error(`Failed to read input file: ${err.message}`, err);
+  }
+};
+
+const [ inputFile = './problem-input.txt' ] = process.argv.slice(2);
+console.log(solve(inputFile));
