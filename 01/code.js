9a6adb42908393073fb81eb4b8efb8d92971e5f5
Callum Robertson <callumullac@hotmail.com>
2022-12-01 11:22:34 +0000

    Refactor day 1 solution to a more readable format

--- 01/code.js
+++ 01/code.js
@@ -1,24 +1,38 @@
 const fs = require('fs');
 
-const solve = (inputFile, topN) => {
-  const data = fs.readFileSync(inputFile, 'utf8');
-
+const read = (filePath) => {
+  const data = fs.readFileSync(filePath, 'utf8');
   // an integer followed by either a newline or the end of the file
   const itemPattern = new RegExp('\\d+(\\n|$)', 'g');
-
   // an inventory is just multiple items in a sequence without any line break between them
   const inventoryPattern = new RegExp(`(${itemPattern.source})+`, 'g');
 
-  return data.match(inventoryPattern)
-    .map(inventory => inventory
-      .match(itemPattern)
-      .map(item => parseInt(item))
-      .reduce((a,b) => a + b))
-    .sort((a,b) => b - a)
-    .slice(0, topN)
-    .reduce((a,b) => a + b)
+  return {
+    "inventories": data.match(inventoryPattern)
+      .map(inventory => ({
+        "items": inventory.match(itemPattern)
+          .map(item => ({
+            "calories": parseInt(item)
+          }))
+      }))
+  };
+};
+
+const solve = (scenario) => {
+  const totalCalories = (inventory) => inventory.items.reduce((acc, item) => acc + item.calories, 0);
+  const topN = (sortedArr, amount) => sortedArr.slice(0, amount);
+  const sum = (arr) => arr.reduce((a, b) => a + b);
+
+  const inventoryCaloriesInDescOrder = scenario.inventories
+    .map(totalCalories)
+    .sort((a , b) => b - a);
+
+  return [
+    sum(topN(inventoryCaloriesInDescOrder, 1)),
+    sum(topN(inventoryCaloriesInDescOrder, 3))
+  ];
 };
 
 const [ inputFile = `${__dirname}/puzzle-input.txt` ] = process.argv.slice(2);
-console.log(solve(inputFile, 1));
-console.log(solve(inputFile, 3));
+const scenario = read(inputFile);
+console.log(solve(scenario));
